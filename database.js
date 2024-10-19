1b1c017861b3845c16386522397343d8a11144ff
Oleksandr Fesiuk <sasha.fesyuk@gmail.com>
2024-10-19 17:05:18 +0300

    initial forum

---
+++ database.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const {MongoClient, ServerApiVersion} = require("mongodb");
+const uri = "mongodb+srv://antony:robocode1234@cluster0.jkl6aib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("forum");
+
+        module.exports = {
+            isUserExist: async (login) => {
+                const user = await db.collection('users').findOne({username: login});
+                return user !== null;
+            },
+            addUser: async (user) => {
+                const salt = crypto.randomBytes(16).toString('hex');
+                const password = crypto.pbkdf2Sync(user.password, salt, 1000, 64, 'sha512').toString('hex');
+
+                await db.collection('users').insertOne({
+                    username: user.login,
+                    password: password,
+                    salt: salt
+                });
+            }
+        };
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
---
+++ server.js
@@ -0,0 +1,92 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const db = require('./database');
+const cookie = require('cookie');
+
+const validAuthTokens = [];
+
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+// const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const authFile = fs.readFileSync(path.join(__dirname, 'static', 'auth.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const registerFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
+// const loginFile = fs.readFileSync(path.join(__dirname, 'static', 'login.html'));
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        switch (req.url) {
+            case '/register':
+                return res.end(registerFile);
+            // case '/login': return res.end(loginFile);
+            case '/auth.js':
+                return res.end(authFile);
+            case '/style.css':
+                return res.end(styleFile);
+            default:
+                return guarded(req, res);
+        }
+    }
+    if (req.method === 'POST') {
+        switch (req.url) {
+            case '/api/register':
+                return registerUser(req, res);
+            // case '/api/login':
+            //     return login(req, res);
+            default:
+                return guarded(req, res);
+        }
+    }
+});
+
+function guarded(req, res) {
+    const credentionals = getCredentionals(req.headers?.cookie);
+    if (!credentionals) {
+        res.writeHead(302, {'Location': '/register'});
+        return res.end();
+    }
+    if (req.method === 'GET') {
+        switch (req.url) {
+            case '/':
+                return res.end(indexHtmlFile);
+            // case '/script.js':
+            //     return res.end(scriptFile);
+        }
+    }
+    res.writeHead(404);
+    return res.end('Error 404');
+}
+
+function getCredentionals(c = '') {
+    const cookies = cookie.parse(c);
+    const token = cookies?.token;
+    if (!token || !validAuthTokens.includes(token)) return null;
+    const [user_id, login] = token.split('.');
+    if (!user_id || !login) return null;
+    return {user_id, login};
+}
+
+function registerUser(req, res) {
+    let data = '';
+    req.on('data', function (chunk) {
+        data += chunk;
+    });
+    req.on('end', async function () {
+        try {
+            const user = JSON.parse(data);
+            if (!user.login || !user.password) {
+                return res.end('Empty login or password');
+            }
+            if (await db.isUserExist(user.login)) {
+                return res.end('User already exist');
+            }
+            await db.addUser(user);
+            return res.end('Registeration is successfull');
+        } catch (e) {
+            res.writeHead(500);
+            return res.end('Error: ' + e);
+        }
+    });
+}
+
+server.listen(3000);
