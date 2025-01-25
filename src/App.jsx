4aad296812f09f27892c0b1179c477fab944fdd4
theantonyis <kostanuka@gmail.com>
2025-01-25 15:31:35 +0200

    addded discussion form to main page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import RegisterForm from './components/RegisterForm'; // Import the RegisterForm
 import LoginForm from './components/LoginForm'; // Import the LoginForm component
 import Header from './components/Header';
 import DiscussionForm from "./components/DiscussionForm";
+import Discussion from "./components/Discussion";
 
 function App() {
     return (
         <Router>
             <Header />
             <Routes>
+                <Route path="/" element={<Discussion />} />
                 <Route path="/register" element={<RegisterForm />} />
                 <Route path="/login" element={<LoginForm />} />
             </Routes>
