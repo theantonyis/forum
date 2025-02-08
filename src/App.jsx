db18fb5294a14bfdd47f2ee41cc3d76e609c83c1
Martinyuk Illia <pidnote@gmail.com>
2025-02-08 15:31:14 +0200

    added new styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm'; // Import the RegisterForm component
 import LoginForm from './components/LoginForm'; // Import the LoginForm component
 import Header from './components/Header';
-import DiscussionForm from "./components/DiscussionForm";
 import Discussion from "./components/Discussion";
 
 function App() {
